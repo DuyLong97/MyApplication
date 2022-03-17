13e3856f9a3e02adddd20d684657fe89c0b794b1
duylong.p <duylong.p@samsung.com>
2022-03-17 17:45:27 +0700

    test 1

--- app/src/main/java/com/example/myapplication/Test.kt
+++ app/src/main/java/com/example/myapplication/Test.kt
@@ -1,23 +1,97 @@
 package com.example.myapplication
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeoutOrNull
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.pow
 
-fun main(){
-    runBlocking {
-        a()
-    }
+fun main() {
+//    println(reverse("-1221"))
+    println(intToRoman(400))
 }
 
-suspend fun a() {
-    var count = 0
-    withTimeoutOrNull(4000) {
-        while (true) {
-            delay(500)
-            println(++count)
-        }
+fun reverse(string: String): Boolean {
+    val charArray = string.toCharArray()
+    val stack = Stack<Char>()
+    val arrayReverse = CharArray(charArray.size)
+
+    for (element in charArray) {
+        stack.push(element)
     }
-    println("End")
+    for (i in charArray.indices) {
+        arrayReverse[i] = stack.pop()
+    }
+
+    return String(arrayReverse) == string
+}
+
+fun intToRoman(num: Int): String {
+    val arrayString = num.toString().toCharArray()
+    val size = arrayString.size
+    var number: Int
+    val array = arrayListOf<Int>()
+    var numberString = ""
+
+    for (i in 0 until size) {
+        number = arrayString[i].toString().toInt() * (10.toDouble().pow((size - 1 - i).toDouble())).toInt()
+        array.add(number)
+    }
+
+    array.forEach { numberString += check(it) }
+    return numberString
+}
+
+fun check(num: Int): String {
+    val list = listOf(1, 5, 10, 50, 100, 500, 1000)
+    val listUnitNum = listOf(1, 5, 10)
+
+    return if (num in list) convert(num)
+    else {
+        val size = num.toString().toCharArray().size
+        var romanNumberString = ""
+        if (size > 1) {
+            val romanNumberInt = (10.toDouble().pow((size - 1).toDouble())).toInt()
+            val quantityRoman = num / romanNumberInt
+
+            for (i in 0 until quantityRoman) {
+                romanNumberString += convert(romanNumberInt)
+            }
+            println("roman $romanNumberString")
+        } else {
+            val max = listUnitNum.first { num < it }
+            when {
+                max - num == 1 -> romanNumberString = convert(1) + convert(max)
+                max - num > 1 -> {
+                    when (num) {
+                        in 5..8 -> {
+                            romanNumberString += convert(5)
+                            for (i in 0 until abs(5 - num)) {
+                                romanNumberString += convert(1)
+                            }
+                        }
+                        in 1..3 -> {
+                            for (i in 0 until num) {
+                                romanNumberString += convert(1)
+                            }
+                        }
+                        else -> romanNumberString = convert(1) + convert(max)
+                    }
+                }
+            }
+        }
+        romanNumberString
+    }
+
+}
+
+fun convert(num: Int): String {
+    return when (num) {
+        1 -> "I"
+        5 -> "V"
+        10 -> "X"
+        50 -> "L"
+        100 -> "C"
+        500 -> "D"
+        else -> "M"
+    }
 }
 
